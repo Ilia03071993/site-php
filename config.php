7cc7e047f95dbf80322ac5b37a4d40087d9fbdfd
ilia03071993 <celiwan@mail.ru>
2020-11-01 07:13:11 +0300

    Началао разработки

--- config.php
+++ config.php
@@ -7,3 +7,4 @@ define("DB","apple");
 
 $connection = @mysqli_connect(DBHOST,DBUSER,DBPASS,DB) or die("Нет соединения с БД");
 mysqli_set_charset($connection, "utf8") or die ("Не установлна кодировка соединения");
+ 
--- index.php
+++ index.php
@@ -1,2 +1,22 @@
 <?php
 include 'config.php';
+include 'functions.php';
+?>
+<!doctype html>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Каталог</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<a href="/catalog/">Главная</a>
+<div class="wrapper">
+    <div class="sidebar">SIDEBAR</div>
+    <div class="content">
+      <?php print_r(); ?>
+    </div>
+</div>
+</body>
+</html>
