d60b912e71a03cf4213b9464217a5faa6651d9de
ilia03071993 <celiwan@mail.ru>
2020-11-02 07:06:50 +0300

    Подготовка проекта продолжение

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
 include 'config.php';
 include 'functions.php';
+
+ 
+$categories = get_cat();
+$categories_tree = map_tree($categories);
+$categories_menu = categories_to_string($categories_tree);
+
+
 ?>
-<!doctype html>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -13,10 +20,21 @@ include 'functions.php';
 <body>
 <a href="/catalog/">Главная</a>
 <div class="wrapper">
-    <div class="sidebar">SIDEBAR</div>
-    <div class="content">
-      <?php print_r(); ?>
+    <div class="sidebar">
+        <ul class="category">
+            <?php echo $categories_menu ?>
+        </ul>
+    </div>
+     <div class="content">
     </div>
 </div>
+<script src="js/jquery-1.9.0.min.js"></script>
+<script src="js/jquery.accordion.js"></script>
+<script src="js/jquery.cookie.js"></script>
+<script>
+  $(document).ready(function)(){
+    $(".category").dcAccordion();
+  });
+</script>
 </body>
 </html>
